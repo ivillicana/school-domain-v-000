e3e24c69b8cd0c84f4dd0c6b8ca933637561a499
Isaac Villicana <villicana.i@gmail.com>
2018-04-12 15:20:50 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,4 +4,11 @@ class School
     @name = name
     @roster = {}
   end
+  def add_student(student, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = student
+    end
+  end
 end
